c9d89c00351b0134987f7fec2863c7e82589faec
patwang123 <patrickwang86@gmail.com>
2020-12-05 23:32:19 -0500

    top words feature

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 # bot.py
+from collections import Counter
 import os
+
+
 
 import discord
 from dotenv import load_dotenv
@@ -10,6 +13,7 @@ GUILD = os.getenv('SERVER_NAME')
 
 client = discord.Client()
 
+stored_messages = {}
 
 @client.event
 async def on_ready():
@@ -23,6 +27,53 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'HELLO {member.name} NICE TO MEET YOU')
 
+@client.event
+async def on_message(message):
+    if message.author == client.user: #don't do self-messages
+        return
+    
+    tokenized_messages = store_message(message)
 
+    await message.channel.send(message.content)
+    if message.content[0] == '!':
+        if tokenized_messages[0].lower() == '!talk':
+            await message.channel.send('HELLO IM PATBOT AND THIS IS AN AUTOMESSAGE FOR !TALK')
+        else:
+            raise discord.DiscordException
+    elif message.content[0] == '?':
+        if tokenized_messages[0].lower() == '?topwords':
+            await message.channel.send(
+                            get_top_words(
+                                stored_messages[message.author],5))
+        else:
+            raise discord.DiscordException
+
+def store_message(message):
+    message.content = message.content.strip(' ')
+    tokenized_messages = message.content.split(' ')
+    if message.author in stored_messages:
+        user = stored_messages[message.author]
+        for word in tokenized_messages:
+            if word in user:
+                user[word] += 1
+            else:
+                user[word] = 1
+    else:
+        stored_messages[message.author] = dict.fromkeys(tokenized_messages, 1)
+    return tokenized_messages
+def get_top_words(words,size):
+    return Counter(words).most_common(size)
+
+@client.event
+async def on_error(event, *args, **kwargs):
+    with open('error.log','a') as f:
+        if event == 'on_message':
+            f.write(f'Unhandled message: {args[0]}\n')
+        else:
+            raise
 client.run(TOKEN)
